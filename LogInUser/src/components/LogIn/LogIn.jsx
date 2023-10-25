5a53d276d1bf4e5299566d9578ded1d90487e941
Eggcanoe <tommyhalleland@hotmail.com>
2023-10-25 11:45:56 +0200

    Started on the log in function

--- LogInUser/src/components/LogIn/LogIn.jsx
+++ LogInUser/src/components/LogIn/LogIn.jsx
@@ -2,3 +2,18 @@ import React from "react";
 import { createContext, useContext, useState } from "react";
 
 const CurrentUserContext = createContext(null);
+
+//Creates a log in function for a static(hard coded) user profile
+export default function LogIn() {
+  const [currentUser, setCurrentUser] = useState(null);
+  return (
+    <CurrentUserContext.Providervalue
+      value={{
+        currentUser,
+        setCurrentUser,
+      }}
+    >
+      <Form />
+    </CurrentUserContext.Providervalue>
+  );
+}
