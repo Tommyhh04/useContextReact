9be8698d302ad277246d1c0ed42cfdad467e3064
Tommyhh04 <tommyhh04.kodehode@gmail.com>
2023-10-26 09:35:41 +0200

    Vite not recongnized. Need to ask teacher

--- LogInUserOld/src/App.jsx
+++ LogInUserOld/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import LoginProfile from "./components/LogIn/LogIn";
 
 function App() {
   return (
     <>
       <h1>Test</h1>
+      <LoginProfile />
     </>
   );
 }
